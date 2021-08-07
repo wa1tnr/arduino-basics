148accda0363ac8de1d3f44ecfbf9600afa8214f
wa1tnr <wa1tnr@users.noreply.github.com>
2021-08-07 14:20:26 +0000

    countdown cruftloop

--- avr.d/03-escape_loop.d/escaped/escaped.ino
+++ avr.d/03-escape_loop.d/escaped/escaped.ino
@@ -1,10 +1,12 @@
 // avr - 16 MHz 5V 32u4 ItsyBitsy from Adafruit is test platform 07 August 2021 14:06z
 bool bbb;
+int countdown;
 int timestamped; // hope millis likes this
 void setup() {
     Serial.begin(115200);
     while(!Serial); delay(999);
     bbb = -1; // TRUE
+    countdown++; // initialize
 }
 bool dothing() {
     if (bbb) {
@@ -17,11 +19,14 @@ bool dothing() {
     bbb = -1; // make it true
     return 0;
 }
+void cruftlooped() { }
 void loop() {
     while(dothing());
     Serial.print("Did thing: ");
     Serial.println(timestamped);
     // bbb = 0; // false
     delay(999);
+    countdown++;
+    if (countdown > 5) { countdown = 0; cruftlooped(); }
 }
 // END.

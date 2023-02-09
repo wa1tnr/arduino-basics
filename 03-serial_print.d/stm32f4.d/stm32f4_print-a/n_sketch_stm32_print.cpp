137ece9e7bc6ea61f4a3e18da6faeb96beea5808
wa1tnr <wa1tnr@users.noreply.github.com>
2023-02-09 17:18:05 +0000

    for 09 FEB 2023 STM32F407 Discovery
    	new file:   n_sketch_stm32_print.cpp
    	new file:   stm32f4_print-a.ino
    
    On branch dvlp-aa-

---
+++ 03-serial_print.d/stm32f4.d/stm32f4_print-a/n_sketch_stm32_print.cpp
@@ -0,0 +1,104 @@
+// Thu  9 Feb 17:12:17 UTC 2023
+
+// initial testing for git-hosted code
+
+// arduino-basics/03-NEW_SERIAL_CONN_print.d/stm32f4.d/stm32f4_print-a/n_sketch_stm32_print.cpp
+
+// stm32f407 discovery - basic blinks arduino ide driven development
+
+//  reasonable bounds on initializing the port pins in a for loop construct. ;)
+
+//  st-flash  write  sketch_feb09a.ino.bin  0x08000000
+//  st-flash  reset
+
+#include <Arduino.h>
+
+#define TESTING 5 // maximum: 5  minimum: 0
+
+// #define NEW_SERIAL_CONN SerialUSB
+#define NEW_SERIAL_CONN SerialUSB
+
+// board edge:
+const int EDGE_GREEN = PA_9;
+const int EDGE_RED   = PD_5;
+
+// cluster:
+const int GREEN  = PD_12;
+const int ORANGE = PD_13;
+const int RED    = PD_14;
+const int BLUE   = PD_15;
+
+const int memory[] = { EDGE_GREEN, EDGE_RED, GREEN, ORANGE, RED, BLUE };
+
+const int memMAX = 6 - 1; // number of elements in array, zero-referenced
+
+const int SELECTED_TEST_PIN = TESTING;
+const int thisPIN = memory[SELECTED_TEST_PIN]; // selects LED
+
+const int ON_TIME = 4 ; // milliseconds
+const int OFF_TIME = 1400;
+
+
+void cpl(int gpio_pin) { // toggle
+  bool state = digitalRead(gpio_pin);
+  state = !state;
+  digitalWrite(gpio_pin, state);
+}
+
+
+void blink() {
+  cpl(thisPIN);
+  delay(ON_TIME);
+  cpl(thisPIN);
+  delay(OFF_TIME);
+}
+
+
+void gpio_setup() {
+  int count = memMAX + 1;
+  for (int nowPIN = 0; nowPIN < count; nowPIN++) {
+    pinMode(memory[nowPIN], 1); // output
+  }
+  digitalWrite(thisPIN, 0);
+  cpl(EDGE_RED);
+}
+
+void serial_setup() {
+  NEW_SERIAL_CONN.begin(9600);
+}
+
+#define PRINT_ITER 333 // how many times the message prints consecutively
+#define SER_MSG "Thu  9 Feb 17:12:17 UTC 2023"
+
+void logon_message() {
+  int iterations = PRINT_ITER ;
+  for (int index = PRINT_ITER; index > 0; index--) {
+    NEW_SERIAL_CONN.println(SER_MSG);
+    delay(500);
+  }
+}
+
+#define WANT_LOGON
+#ifdef WANT_LOGON
+  const int WANTED_LOGON = -1;
+#endif
+#ifndef WANT_LOGON
+  const int WANTED_LOGON = 0;
+#endif
+
+void setup() {
+  delay(800);
+  gpio_setup();
+  serial_setup();
+  if (WANTED_LOGON) { logon_message(); }
+  int blinks = 11;
+  for (int i = blinks; i > 0; i--) {
+    blink();
+  }
+}
+
+void loop() {
+  // empty
+}
+
+// END.
---
+++ 03-serial_print.d/stm32f4.d/stm32f4_print-a/stm32f4_print-a.ino
@@ -0,0 +1,7 @@
+// Thu  9 Feb 16:03:22 UTC 2023
+
+// arduino-basics/03-serial_print.d/stm32f4.d/stm32f4_print-a/n_sketch_stm32_print.cpp
+
+// stm32f407 discovery - basic blinks arduino ide driven development
+
+// END.
